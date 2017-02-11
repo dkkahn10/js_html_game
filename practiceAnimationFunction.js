de48fc71a152e0e6f6b1b2d33b6389e1e38afe12
Dustin Kahn <dustin@dustinkahn.com>
2017-02-11 09:53:26 -0500

    moving forward in animation window

--- practiceAnimationFunction.js
+++ practiceAnimationFunction.js
@@ -7,5 +7,15 @@
     window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
   }
 
+  if(!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function(callback, element) {
+      let currTime = new Date().getTime();
+      let timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      let id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  };
+
   
 })
