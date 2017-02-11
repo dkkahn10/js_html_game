f73f2f5b876a2bc2217bd6798fb5198d2aced57e
Dustin Kahn <dustin@dustinkahn.com>
2017-02-11 09:48:39 -0500

    animation window created

---
+++ practiceAnimationFunction.js
@@ -0,0 +1,11 @@
+(function () {
+  let lastTime = 0;
+  let vendors = ['ms', 'moz', 'webkit', 'o'];
+
+  for(let x = 0; x < vendors.length && !window.requestAnimationFrame; x++) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
+
+  
+})
