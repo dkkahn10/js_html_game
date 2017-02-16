d9760807559faf015223e498a441b7d30c53afb9
Dustin Kahn <dustin@dustinkahn.com>
2017-02-16 17:15:59 -0500

    game layers hidden, game start shown, using jQuery

--- js/game.js
+++ js/game.js
@@ -1,5 +1,9 @@
 let game = {
   init: function() {
-    
+    $('.gamelayer').hide();
+    $('#gamestartscreen').show();
+
+    game.canvas = $('#gamecanvas')[0];
+    game.context = game.canvas.getContext('2d');
   }
 }
