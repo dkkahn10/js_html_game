e2c9557cec0565399d4b3dfd32919e41abcb82a1
Dustin Kahn <dustin@dustinkahn.com>
2017-03-04 14:19:44 -0500

    finished errored requestAnimationFrame conditional

--- js/game.js
+++ js/game.js
@@ -9,8 +9,15 @@
 
   if (!window.requestAnimationFrame) {
     window.requestAnimationFrame = function(callbaack, element) {
-      
+      let currTime = new Date().getTime();
+      let timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      let id = window.setTimeout(function() { callback(currTime + timeToCall); },
+    timeToCall);
+    lastTime = currTime + timeToCall;
+    return id;
     }
+
+    
   }
 
 })
