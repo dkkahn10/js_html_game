654c34464768c60f84bcebfc8c11ef5e52a866d0
Dustin Kahn <dustin@dustinkahn.com>
2017-03-21 14:22:14 -0400

    start using debug draw in our world

--- box2d.js
+++ box2d.js
@@ -50,4 +50,6 @@ function setupDebugDraw() {
   debugDraw.SetFillAlpha(0.3);
   debugDraw.SetLineThickness(1.0);
   debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+
+  world.SetDebugDraw(debugDraw);
 }
