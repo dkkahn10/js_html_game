b579509241a1e9b4e5df8a1770afdce7a0800b17
Dustin Kahn <dustin@dustinkahn.com>
2017-03-21 14:21:21 -0400

    display all shapes and joints

--- box2d.js
+++ box2d.js
@@ -49,4 +49,5 @@ function setupDebugDraw() {
   debugDraw.SetDrawScale(scale);
   debugDraw.SetFillAlpha(0.3);
   debugDraw.SetLineThickness(1.0);
+  debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
 }
